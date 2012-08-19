4e813f7750510f50b203f89660f7201c2fe501cc
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2012-08-19 16:42:55 -0700

    add alias be and ber

--- bashrc.bash
+++ bashrc.bash
@@ -1,5 +1,8 @@
 set -o vi
 PS1="\u\[$(tput sgr0)\]@\h: \[$(tput setaf 2)\]\w\[$(tput sgr0)\]\n\[$(tput setaf 7)\]$\[$(tput sgr0)\] "
 
+# aliases
 alias cl='clear'
 alias ll='ls -lh'
+alias be='bundle exec'
+alias ber='bundle exec rspec'
